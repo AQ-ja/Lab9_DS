9edeee1952a1bd8334d45b1894baef5097658300
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-10-17 20:31:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ option = {
         "type": "category",
         "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
     },
-    "yAxis": {"type": "value"},
-    "series": [{"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}],
+    "yAxis": { "type": "value" },
+    "series": [
+        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line" }
+    ],
 }
-st_echarts(
-    options=option, height="400px",
-)
