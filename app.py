e98d18132c28e4ef70829a32519e011638f0677c
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-10-17 18:50:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-import streamlit as st 
-from streamlit_echarts import *
+import altair as alt
+import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write("VAMOS A EMPEZAR")
+from streamlit_vega_lite import vega_lite_component, altair_component
 
-options = {
-    "xAxis": {
-        "type": "category",
-        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    "yAxis": {"type": "value"},
-    "series": [
-        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
-    ],
-}
-st_echarts(options=options)
+hist_data = pd.DataFrame(np.random.normal(42, 10, (200, 1)), columns=["x"])
+
+@st.cache
+def altair_histogram():
+    brushed = alt.selection_interval(encodings=["x"], name="brushed")
+
+    return (
+        alt.Chart(hist_data)
+        .mark_bar()
+        .encode(alt.X("x:Q", bin=True), y="count()")
+        .add_selection(brushed)
+    )
+
+event_dict = altair_component(altair_chart=altair_histogram())
+
+r = event_dict.get("x")
+if r:
+    filtered = hist_data[(hist_data.x >= r[0]) & (hist_data.x < r[1])]
+    st.write(filtered)
