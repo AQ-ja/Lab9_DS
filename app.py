54dd45f3b9d77a96a75008e8bc44a47584a5f87c
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-10-17 18:42:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
 from ctypes import windll
 from curses import window
 import streamlit as st 
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from streamlit_echarts import st_pyecharts
 
 st.write("VAMOS A EMPEZAR")
 
-st.write(m.run(window=15))
+
+
+b = (
+    Bar()
+    .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+    .add_yaxis(
+        "2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2]
+    )
+    .set_global_opts(
+        title_opts=opts.TitleOpts(
+            title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+        ),
+        toolbox_opts=opts.ToolboxOpts(),
+    )
+)
+st_pyecharts(b)
+
+
+from streamlit_echarts import st_echarts
+
+options = {
+    "xAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "yAxis": {"type": "value"},
+    "series": [
+        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
+    ],
+}
+st_echarts(options=options)
